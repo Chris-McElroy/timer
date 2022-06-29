821a7be32486370fb937f4a86ed5cd42470b3753
ChrisMcElroy <csmcelroy@me.com>
2022-06-28 21:41:50 -0700

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Chris McElroy on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
